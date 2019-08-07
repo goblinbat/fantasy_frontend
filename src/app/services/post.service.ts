207606afe6f7ab8439f2408b5718ab29083932a6
Molly Hopper <mollyhopper95@gmail.com>
2019-08-07 17:17:59 -0400

    built out comment service and remaining post service functions

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -8,30 +8,40 @@ import { Post } from '../models/post.model';
   providedIn: 'root'
 })
 export class PostService {
-  private baseUrl: 'http://localhost:3000/post'
+  baseUrl: 'http://localhost:3000'
 
   constructor(private http: HttpClient) { }
 
   // get all posts, regardless of user (use method in authcontroller)
+  getAllPosts() {
+    return this.http.get(`${this.baseUrl}/auth/all`)
+  }
 
   // get all of a user's posts
   getPosts(): Observable<Post[]> {
-    return this.http.get<Post[]>(this.baseUrl)
+    return this.http.get<Post[]>(`${this.baseUrl}/post`, {headers: this.setHeader()})
   }
 
   // get a single post
   getPost(id: number): Observable<Post[]> {
-    const url = `${this.baseUrl}/${id}`
+    const url = `${this.baseUrl}/post/${id}`
     return this.http.get<Post[]>(url)
   }
 
   // create post
-
+  createPost(newPost: any) {
+    return this.http.post(`${this.baseUrl}/post`, newPost, {headers: this.setHeader()} )
+  }
 
   // update post
-
+  updatePost(id:number, editPost: any) {
+    return this.http.put(`${this.baseUrl}/post/${id}`, editPost, {headers: this.setHeader()})
+  }
 
   // delete post
+  deletePost(id:number) {
+    return this.http.delete(`${this.baseUrl}/post/${id}`, {headers: this.setHeader()})
+  }
 
   private setHeader(): HttpHeaders {
     return new HttpHeaders().set('authorization', `${localStorage.getItem('access_token')}`);
