6b9bb2241279caf0dcf9e4758c4427ad1400d65a
Molly Hopper <mollyhopper95@gmail.com>
2019-08-20 13:18:58 -0400

    not hitting backend...

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,14 +2,14 @@ export const environment = {
   production: true
 };
 
-export let baseUrl = 'https://mhpbbd-fantasy-server.herokuapp.com/';
+export let baseUrl = 'https://mhpbbd-fantasy-server.herokuapp.com';
 
 switch (window.location.hostname) {
   case 'localhost' || '127.0.0.1':
       baseUrl = 'http://localhost:3000';
       break;
   case 'https://fantasy-forum.herokuapp.com/' : 
-      baseUrl = 'https://mhpbbd-fantasy-server.herokuapp.com/';
+      baseUrl = 'https://mhpbbd-fantasy-server.herokuapp.com';
       break;
 }
 
